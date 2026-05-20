c1300f359e5b15d19f5ff6236c91d6d4d7a9cef4
meetpatel7247 <meetjpatel2406@gmail.com>
2026-05-20 23:59:47 +0530

    chore: restore event-management subpath

--- App/vite.config.js
+++ App/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Base path for root domain
+  base: '/event-management/', // Base path for GitHub Project Pages
   plugins: [react()],
   resolve: {
     alias: {
