07a4c970aabdf80f428ef7d2e5250f22cbe7bc95
meetpatel7247 <meetjpatel2406@gmail.com>
2026-05-21 00:00:47 +0530

    chore: add basename to BrowserRouter for subpath routing

--- App/src/main.jsx
+++ App/src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
     {/* Redux Provider for global state management */}
     <Provider store={store}>
       {/* BrowserRouter for handling client-side routing */}
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </Provider>
